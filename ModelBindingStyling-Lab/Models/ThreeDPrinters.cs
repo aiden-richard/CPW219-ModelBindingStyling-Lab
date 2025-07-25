f2a15b882d91031dffa2c297a49f8ed216fe8588
Aiden Richard <115966042+aiden-richard@users.noreply.github.com>
2025-07-24 18:39:56 -0700

    Initial commit

---
+++ ModelBindingStyling-Lab/Models/ThreeDPrinters.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelBindingStyling_Lab.Models
+{
+    /// <summary>
+    /// Represents an individual 3D printer
+    /// </summary>
+    public class ThreeDPrinters
+    {
+        /// <summary>
+        /// Stock Keeping Unit identifier
+        /// </summary>
+        public required string SKU { get; set; }
+
+        /// <summary>
+        /// Title of the printer product
+        /// </summary>
+        public required string Title { get; set; }
+
+        /// <summary>
+        /// Sale price
+        /// </summary>
+        [DataType(DataType.Currency)]
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Build volume that can be printed
+        /// ex. 180 x 180 x 180 mm
+        /// </summary>
+        [Display(Name = "Build Volume")]
+        public required string BuildVolume { get; set; }
+
+        /// <summary>
+        /// Product image of the printer
+        /// </summary>
+        [Display(Name = "Printer Image")]
+        public string PrinterImage { get; set; } = "https://placehold.co/600x400";
+    }
+}
---
+++ ModelBindingStyling-Lab/Models/UserProfile.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ModelBindingStyling_Lab.Models
+{
+    /// <summary>
+    /// Individual user profile information
+    /// </summary>
+    public class UserProfile
+    {
+        public int UserProfileId { get; set; }
+
+        /// <summary>
+        /// First and last legal name of the user
+        /// </summary>
+        [Display(Name = "Full Name")]
+        public required string FullName { get; set; }
+
+        /// <summary>
+        /// User date of birth
+        /// </summary>
+        [Display(Name = "Date of birth")]
+        public DateOnly DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Contact email
+        /// </summary>
+        [Display(Name = "Email Address")]
+        public required string Email { get; set; }
+
+        /// <summary>
+        /// Contact phone number
+        /// </summary>
+        [Display(Name = "Phone Number")]
+        public required string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Profile image of the user
+        /// </summary>
+        [Display(Name = "Image URL")]
+        public required string ImageUrl { get; set; }
+
+        /// <summary>
+        /// The users GitHub profile URL
+        /// </summary>
+        [Display(Name = "GitHub URL")]
+        public required string GitHubUrl { get; set; }
+
+        /// <summary>
+        /// List of languages the user is skilled in. Will be an empty
+        /// list if there are 0
+        /// </summary>
+        [Display(Name = "Skilled Languages")]
+        public List<string> SkilledLanguages { get; set; } = [];
+    }
+}
